1bd90ba4ed125617c59a98c5bb38dca244042272
shashmahawar <shshnkmhwr@gmail.com>
2022-12-17 22:55:06 +0530

    Coming Soon Pop-Up

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -6,3 +6,13 @@ window.onscroll = function() {
         document.getElementsByTagName("nav")[0].style.background = "rgba(248, 248, 248, 0.8)";
     }
 };
+
+function preAlertClose() {
+    document.getElementById("pre-alert").style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById("pre-alert")) {
+        document.getElementById("pre-alert").style.display = "none";
+    }
+}
